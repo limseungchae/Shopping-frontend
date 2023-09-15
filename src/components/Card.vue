7933b295bd6d4f9ed4b2c7c8533194eed05f4043
admin <tmdco12@naver.com>
2023-09-16 02:35:26 +0900

    백엔드 items 값이 사진에 들어감

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -17,6 +17,9 @@
 <script>
 export default {
   name:"Card",
+  props:{
+    item: Object
+  }
 }
 </script>
 
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,7 +5,8 @@
       <div class="row py-lg-5">
         <div class="col-lg-6 col-md-8 mx-auto">
           <h1 class="fw-light">Album example</h1>
-          <p class="lead text-body-secondary">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+          <p class="lead text-body-secondary">Something short and leading about the collection below—its contents, the
+            creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
           <p>
             <a href="#" class="btn btn-primary my-2">Main call to action</a>
             <a href="#" class="btn btn-secondary my-2">Secondary action</a>
@@ -18,8 +19,9 @@
       <div class="container">
 
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-          <div class="col" v-for="i in 12" :key="i">
-            <Card/>
+          <div class="col" v-for="(item, idx) in  state.items" :key="idx">
+            {{item}}
+            <Card :item="item"/>
           </div>
         </div>
       </div>
@@ -29,17 +31,24 @@
 </template>
 
 <script>
-import Card from "@/components/Card.vue";
 import axios from "axios";
+import {reactive} from "vue";
+import Card from "@/components/Card.vue";
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Main',
   components: {Card},
   setup() {
-    axios.get("/api/items").then((res)=> {
-      console.log(res);
+    const state = reactive({
+      items: []
     })
+
+    axios.get("/api/items").then(({data}) => {
+      state.items = data;
+    })
+
+    return {state}
   }
 }
 </script>
