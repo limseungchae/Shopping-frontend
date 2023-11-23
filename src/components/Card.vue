1cc8a0d1a406fdec2f555db64ec03d0cd1ade2ff
admin <tmdco12@naver.com>
2023-11-23 10:45:23 +0900

    card 데이터 삽입

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,28 +1,48 @@
 <template>
   <div class="card shadow-sm">
-    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+    <span class="img" :style="{backgroundImage: `url(${item.imgPath})`}" />
     <div class="card-body">
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text">{{ item.name }}</p>
       <div class="d-flex justify-content-between align-items-center">
-        <div class="btn-group">
-          <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-          <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-        </div>
-        <small class="text-body-secondary">9 mins</small>
+        <button class="btn btn-primary">구입하기</button>
+        <small class="price text-muted">
+          {{ lib.getNumberFormatted(item.price) }}원
+        </small>
+        <span class="discount badge bg-danger">
+          {{ item.discount }}%
+        </span>
+        <small class="real text-danger">
+          {{ lib.getNumberFormatted(item.price - (item.price * item.discount / 100)) }}%
+        </small>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import lib from "@/scripts/lib";
+
 export default {
-  name:"Card",
-  props:{
+  name: "Card",
+  props: {
     item: Object
+  },
+  setup() {
+    return {lib}
   }
 }
 </script>
 
 <style scoped>
+.card .img{
+  display: inline-block;
+  width: 100%;
+  height: 250px;
+  background-size: cover;
+  background-position: center;
+}
 
+.card .card-body .price {
+  text-decoration: line-through;
+}
 </style>
